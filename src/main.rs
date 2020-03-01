fea417be72f6856f5a3d12168a5a7bc0bcdb0589
kz_morita <kz.morit@gmail.com>
2020-03-01 19:48:28 +0900

    Second step

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,24 @@
-use actix_web::{web, App, HttpServer, HttpResponse, Error};
+use actix_web::{web, App, HttpServer, HttpResponse, Error, error};
+use tera::Tera;
 
-async fn index() -> Result<HttpResponse, Error> {
-    Ok(HttpResponse::Ok().content_type("text/plain").body("Hello World"))
+async fn index(tmpl: web::Data<Tera>) -> Result<HttpResponse, Error> {
+
+    let mut ctx = tera::Context::new();
+    ctx.insert("name", "kz_morita");
+    let view =
+        tmpl.render("index.html.tera", &ctx)
+            .map_err(|e| error::ErrorInternalServerError(e))?;
+
+    Ok(HttpResponse::Ok().content_type("text/html").body(view))
 }
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new( || {
+        let templates = Tera::new("templates/**/*").unwrap();
+
         App::new()
+            .data(templates)
             .service(web::resource("/").to(index))
     })
         .bind("localhost:3000")
