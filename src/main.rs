7b04c21b5fdbcf33497ce26850b931af63ff64c3
kz_morita <kz.morit@gmail.com>
2020-03-01 19:23:37 +0900

    Hello World

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, HttpServer, HttpResponse, Error};
+
+async fn index() -> Result<HttpResponse, Error> {
+    Ok(HttpResponse::Ok().content_type("text/plain").body("Hello World"))
+}
+
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new( || {
+        App::new()
+            .service(web::resource("/").to(index))
+    })
+        .bind("localhost:3000")
+        .expect("Can not bind to port 3000")
+        .run()
+        .await
 }
